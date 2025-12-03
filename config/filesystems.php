7115715be5d06c9b2fbd4fc543ecbdac6b2cddf9
wajihahzahrah03-beep <wajihahzahrah03@gmail.com>
2025-12-04 05:06:25 +0700

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -32,19 +32,14 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app/private'),
-            'serve' => true,
-            'throw' => false,
-            'report' => false,
-        ],
+            'root' => storage_path('app'),
+         ],
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => public_path(),
+            'url' => env('APP_URL').'/',
             'visibility' => 'public',
-            'throw' => false,
-            'report' => false,
         ],
 
         's3' => [
